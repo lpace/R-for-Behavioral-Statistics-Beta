96a21aba34eace203e2aee2a0480feb28a0acaba
Larry Pace <pace.larry@gmail.com>
2015-03-13 14:27:13 -0400

    Adding to data structures module

--- Data_Structures/initLesson.R
+++ Data_Structures/initLesson.R
@@ -2,3 +2,8 @@
 # lesson is started. Any variables created here will show up in
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
+
+lesson_dir <- file.path(path.package("swirl"), "Courses",
+                        "R-for-Behavioral-Statistics-Beta", "Data_Structures")
+setwd(lesson_dir)
+#cars <- read.csv("mtcars.csv")
